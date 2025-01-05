52c7934f81ba1cea3c087bc96f2c647ccfea6ca2
José Wolf <wolf.jose89@gmail.com>
2025-01-05 04:49:27 -0300

    Criando uma exceção para tratar valores que sejam menores ou iguais a zero.

---
+++ src/main/java/model/exceptions/ValorNegativoOuIgualAZeroException.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class ValorNegativoOuIgualAZeroException extends Exception{
+    public ValorNegativoOuIgualAZeroException(String mensagem){
+        super(mensagem);
+    }
+
+
+}
