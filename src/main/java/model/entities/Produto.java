5e330e74ed28f7583dd12fdcd00f382b7d56651f
José Wolf <wolf.jose89@gmail.com>
2025-01-05 03:49:34 -0300

    Criando a classe Produto e adicionando funcionalidade que retorna como resultado se possui produto disponível.

--- src/main/java/model/entities/Produto.java
+++ src/main/java/model/entities/Produto.java
@@ -7,6 +7,31 @@ public class Produto {
     private String descricao;
     private boolean disponivel;
 
-    te
+    public Produto(){}
 
+    public Produto(String nome, double valor, String descricao) {
+        this.nome = nome;
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public String  produtoDisponivel(String resultado){
+        return resultado.equalsIgnoreCase("sim") ? "Produto disponível: sim" : "Produto disponível: não";
+    }
 }
