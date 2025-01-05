074954f735c64e06dfb2980851400ef232ae1ec1
José Wolf <wolf.jose89@gmail.com>
2025-01-05 01:26:17 -0300

    Inicializando o repositório.

---
+++ src/main/java/model/entities/Produto.java
@@ -0,0 +1,12 @@
+package model.entities;
+
+public class Produto {
+
+    private String nome;
+    private double valor;
+    private String descricao;
+    private boolean disponivel;
+
+    te
+
+}
