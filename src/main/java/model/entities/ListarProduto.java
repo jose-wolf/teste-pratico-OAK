40554d2205dcd09b251b851d7c10cc01229885e4
José Wolf <wolf.jose89@gmail.com>
2025-01-05 13:33:51 -0300

    Corrigir: corrigindo nome mostrado no console.

--- src/main/java/model/entities/ListarProduto.java
+++ src/main/java/model/entities/ListarProduto.java
@@ -1,4 +1,23 @@
 package model.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListarProduto {
+
+    List<Produto> produtoList = new ArrayList<>();
+
+    public ListarProduto() {
+
+    }
+
+    public void adicionarProduto(Produto produto){
+        produtoList.add(produto);
+    }
+
+    public void listarProdutos(){
+        for (Produto produto : produtoList){
+            System.out.println("Produto: " + produto.getNome() + " - Valor: "  + produto.formatarValor());
+        }
+    }
 }
